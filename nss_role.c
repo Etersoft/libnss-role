fab47b409171a287d6f512c2f7efde91701de52b
Pavel Shilovsky <piastry@altlinux.org>
2009-09-10 20:48:31 +0400

    add error handling

--- nss_role.c
+++ nss_role.c
@@ -9,8 +9,13 @@
 #include <stdio.h>
 #include <string.h>
 
-static int STR_max_size = 10000;
-static int STR_min_size = 100;
+static const int STR_min_size  = 100;
+
+static const int NO_SUCH_GROUP = -100;
+static const int OUT_OF_RANGE  = -101;
+static const int MEMORY_ERROR  = -102;
+static const int UNKNOWN_ERROR = -103;
+static const int OK            = 0;
 
 static pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
 
@@ -26,24 +31,30 @@ struct graph {
 	int size;
 	int capacity;
 };
-static gid_t get_gid(char *);
+static gid_t get_gid(char *, gid_t *);
 
-static void graph_add(struct graph *G, struct ver v)
+static int graph_add(struct graph *G, struct ver v)
 {
 	G->gr[G->size++] = v;
 	if (G->size == G->capacity) {
 		G->capacity *= 2;
 		G->gr = (struct ver *) realloc(G->gr, sizeof(struct ver) * G->capacity);
+		if (!G->gr)
+			return MEMORY_ERROR;
 	}
+	return OK;
 }
 
-static void ver_add(struct ver *v, gid_t g)
+static int ver_add(struct ver *v, gid_t g)
 {
 	v->list[v->size++] = g;
 	if (v->size == v->capacity) {
 		v->capacity *= 2;
 		v->list = (gid_t *) realloc(v->list, sizeof(gid_t) * v->capacity);
+		if (!v->list)
+			return MEMORY_ERROR;
 	}
+	return OK;
 }
 
 struct group_name {
@@ -80,22 +91,26 @@ static int realloc_groups (long int **size, gid_t ***groups, long int limit)
 		realloc((**groups),
 			new_size * sizeof(***groups));
 	if (!new_groups)
-		return 0;
+		return MEMORY_ERROR;
 
 	**groups = new_groups;
 	**size = new_size;
 
-	return 1;
+	return OK;
 }
 
-static void parse_line(char *s, struct graph *G)
+static int parse_line(char *s, struct graph *G)
 {
-//	printf("%s\n", s);
+	int result;
 	unsigned long len = strlen(s);
 	int i;
 	struct group_name role_name = {{0}, 0};
 	struct ver role = {0, 0, 0, 10};
 	role.list = (gid_t *) malloc(sizeof(gid_t) * role.capacity);
+
+	if (!role.list)
+		return MEMORY_ERROR;
+
 	for(i = 0; i < len; i++) {
 		if (s[i] == ':') {
 			i++;
@@ -103,7 +118,11 @@ static void parse_line(char *s, struct graph *G)
 		}
 		role_name.name[role_name.id++] = s[i];
 	}
-	role.gid = get_gid(role_name.name);
+
+	result = get_gid(role_name.name, &role.gid);
+	if (result != OK)
+		return result;
+
 	while(1) {
 		struct group_name gr_name = {{0}, 0};
 		if (i >= len)
@@ -116,29 +135,40 @@ static void parse_line(char *s, struct graph *G)
 			}
 			gr_name.name[gr_name.id++] = s[i];
 		}
-		gr = get_gid(gr_name.name);
-		ver_add(&role, gr);
+
+		result = get_gid(gr_name.name, &gr);
+		if (result != OK && result != NO_SUCH_GROUP)
+			return result;
+		else if (result == NO_SUCH_GROUP)
+			continue;
+
+		result = ver_add(&role, gr);
+		if (result != OK)
+			return result;
 	}
 
-	graph_add(G, role);
-
+	result = graph_add(G, role);
+	return result;
 }
 
-static void reading(char *s, struct graph *G)
+static int reading(char *s, struct graph *G)
 {
+	int result;
 	FILE *f = fopen(s, "r");
 	unsigned long len = STR_min_size;
 	char *str = malloc(len * sizeof(char));
 	unsigned long id = 0;
 	char c;
-	printf("in reading\n");
+
 	while(1) {
 		c = fgetc(f);
 		if (c == EOF)
 			break;
 		if (c == '\n') {
 			str[id] = '\0';
-			parse_line(str, G);
+			result = parse_line(str, G);
+			if (result != OK)
+				return result;
 			id = 0;
 			continue;
 		}
@@ -146,12 +176,17 @@ static void reading(char *s, struct graph *G)
 		if (id == len) {
 			len *= 2;
 			str = realloc(str, len * sizeof(char));
+			if (!str)
+				return MEMORY_ERROR;
 		}
 	}
 	if (id) {
 		str[id] = '\0';
-		parse_line(str, G);
+		result = parse_line(str, G);
+		if (result != OK)
+			return result;
 	}
+	return OK;
 }
 
 static void dfs(gid_t v)
@@ -159,22 +194,24 @@ static void dfs(gid_t v)
 
 }
 
-static gid_t get_gid(char *gr_name)
+static gid_t get_gid(char *gr_name, gid_t *ans)
 {
 	if (isalpha(gr_name[0])) {
 		struct group grp, *grp_ptr;
 		char buffer[1000];
 		if (getgrnam_r(gr_name, &grp, buffer, 1000, &grp_ptr) == 0) {
 			if (errno == ERANGE)
-				return -abs(ERANGE);
+				return OUT_OF_RANGE;
 			if (errno != 0)
-				return -1;
+				return UNKNOWN_ERROR;
 		}
 		if (!grp_ptr)
-			return -2;
-		return grp.gr_gid;
+			return NO_SUCH_GROUP;
+		*ans = grp.gr_gid;
+		return OK;
 	}
-	return (gid_t) atoi(gr_name);
+	*ans = (gid_t) atoi(gr_name);
+	return OK;
 }
 
 
@@ -203,7 +240,15 @@ out:
 int main(void)
 {
 	struct graph G = {0, 0, 10};
+	int i;
 	G.gr = malloc(sizeof(struct ver) * G.capacity);
 	reading("/etc/role", &G);
+	for(i = 0; i < G.size; i++) {
+		int j;
+		printf("%llu: ", G.gr[i].gid);
+		for(j = 0; j < G.gr[i].size; j++)
+			printf("%llu, ", G.gr[i].list[j]);
+		printf("\n");
+	}
 	return 0;
 }
