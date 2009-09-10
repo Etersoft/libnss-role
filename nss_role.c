5217c9862532acd63e5aee8f6be804659b7e32f8
Pavel Shilovsky <piastry@altlinux.org>
2009-09-10 18:42:56 +0400

    reading()

--- nss_role.c
+++ nss_role.c
@@ -6,8 +6,10 @@
 #include <sys/types.h>
 #include <stdlib.h>
 #include <errno.h>
+#include <stdio.h>
 
-
+static int STR_max_size = 10000;
+static int STR_min_size = 100;
 
 static pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
 
@@ -24,7 +26,12 @@ struct graph {
 	int capability;
 };
 
-void free_all(struct graph *G)
+struct string {
+	char s[20];
+	int id;
+};
+
+static void free_all(struct graph *G)
 {
 	int i;
 	for(i = 0; i < G->size; i++) {
@@ -34,8 +41,6 @@ void free_all(struct graph *G)
 		free(&G->gr[i]);
 	}
 }
-
-
 
 static int realloc_groups (long int **size, gid_t ***groups, long int limit)
 {
@@ -63,13 +68,64 @@ static int realloc_groups (long int **size, gid_t ***groups, long int limit)
 	return 1;
 }
 
-void reading(char *s, struct graph *G) {
+static void parse_line(char *s, struct graph *G)
+{
+//	printf("%s\n", s);
+}
+
+static void reading(char *s, struct graph *G)
+{
+	FILE *f = fopen(s, "r");
+	unsigned long len = STR_min_size;
+	char *str = malloc(len * sizeof(char));
+	unsigned long id = 0;
+	char c;
+	printf("in reading\n");
+	while(1) {
+		c = fgetc(f);
+		if (c == EOF)
+			break;
+		if (c == '\n') {
+			str[id] = '\0';
+			parse_line(str, G);
+			id = 0;
+			continue;
+		}
+		str[id++] = c;
+		if (id == len) {
+			len *= 2;
+			str = realloc(str, len * sizeof(char));
+		}
+	}
+	if (id) {
+		str[id] = '\0';
+		parse_line(str, G);
+	}
+}
+
+static void dfs(gid_t v)
+{
 
 }
 
-void dfs(gid_t v) {
-
+static gid_t get_gid(char *role_name)
+{
+	if (isalpha(role_name[0])) {
+		struct group grp, *grp_ptr;
+		char buffer[1000];
+		if (getgrnam_r(role_name, &grp, buffer, 1000, &grp_ptr) == 0) {
+			if (errno == ERANGE)
+				return -abs(ERANGE);
+			if (errno != 0)
+				return -1;
+		}
+		if (!grp_ptr)
+			return -2;
+		return grp.gr_gid;
+	}
+	return atoi(role_name);
 }
+
 
 enum nss_status _nss_role_initgroups_dyn (char *user, gid_t main_group, long int *start, long int *size, gid_t **groups, long int limit, int *errnop)
 {
@@ -88,4 +144,12 @@ out:
 	free_all(&G);
 	pthread_mutex_unlock(&mutex);
 	return ret;
+}
+
+
+int main(void)
+{
+	struct graph G;
+	reading("/etc/role", &G);
+	return 0;
 }
