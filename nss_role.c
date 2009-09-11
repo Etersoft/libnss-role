bc764485672566bbe87be77b763595d83244960f
Pavel Shilovsky <piastry@altlinux.org>
2009-09-11 11:25:44 +0400

    dfs()

--- nss_role.c
+++ nss_role.c
@@ -26,11 +26,15 @@ struct ver {
 	int capacity;
 };
 
+typedef struct ver group_collector;
+
 struct graph {
 	struct ver *gr;
+	int *used;
 	int size;
 	int capacity;
 };
+
 static gid_t get_gid(char *, gid_t *);
 
 static int graph_add(struct graph *G, struct ver v)
@@ -62,18 +66,29 @@ struct group_name {
 	int id;
 };
 
+static int find_id(struct graph *G, gid_t g, int *id)
+{
+	int i;
+	for(i = 0; i < G->size; i++)
+		if (G->gr[i].gid == g) {
+			*id = i;
+			return OK;
+		}
+
+	return NO_SUCH_GROUP;
+}
+
 static void free_all(struct graph *G)
 {
 	int i;
 	for(i = 0; i < G->size; i++) {
-		int j;
-		for(j = 0; j < G->gr[i].size; j++)
-			free(G->gr[i].list);
-		free(&G->gr[i]);
+		free(G->gr[i].list);
 	}
+	free(G->gr);
+	free(G->used);
 }
 
-static int realloc_groups (long int **size, gid_t ***groups, long int limit)
+static int realloc_groups(long int **size, gid_t ***groups, long int limit)
 {
 	long int new_size;
 	gid_t *new_groups;
@@ -189,9 +204,32 @@ static int reading(char *s, struct graph *G)
 	return OK;
 }
 
-static void dfs(gid_t v)
+static int dfs(struct graph *G, gid_t v, group_collector *col)
 {
+	int i, j, result;
 
+	result = find_id(G, v, &i);
+	if (result != OK) {
+		result = ver_add(col, v);
+		return result;
+	}
+
+	//printf("after find %llu\n", v);
+	if (G->used[i])
+		return OK;
+	result = ver_add(col, v);
+	if (result != OK)
+		return result;
+	G->used[i] = 1;
+
+	for(j = 0; j < G->gr[i].size; j++) {
+	//	printf("    dfs: %d %llu \n", j, v);
+		result = dfs(G, G->gr[i].list[j], col);
+	//	printf("    result: %d %llu\n", result, v);
+		if (result != OK && result != NO_SUCH_GROUP)
+			return result;
+	}
+	return OK;
 }
 
 static gid_t get_gid(char *gr_name, gid_t *ans)
@@ -214,23 +252,27 @@ static gid_t get_gid(char *gr_name, gid_t *ans)
 	return OK;
 }
 
-
 enum nss_status _nss_role_initgroups_dyn (char *user, gid_t main_group, long int *start, long int *size, gid_t **groups, long int limit, int *errnop)
 {
 	enum nss_status ret = NSS_STATUS_SUCCESS;
 	pthread_mutex_lock(&mutex);
 
-	struct graph G = {0, 0, 10};
+	struct graph G = {0, 0, 0, 10};
 	int i;
+	group_collector col = {0, 0, 0, 10};
+	col.list = (int *) malloc(sizeof(int) * col.capacity);
 
-	G.gr = malloc(sizeof(struct ver) * G.capacity);
+	G.gr = (struct ver *) malloc(sizeof(struct ver) * G.capacity);
 	reading("/etc/role", &G);
+	G.used = (int *) malloc(sizeof(int) * G.capacity);
+	memset(G.used, 0, sizeof(int) * G.capacity);
 
-	dfs(main_group);
+	dfs(&G, main_group, &col);
 	for(i = 0; i < *start; i++)
-		dfs((*groups)[i]);
+		dfs(&G, (*groups)[i], &col);
 
 out:
+	free(col.list);
 	free_all(&G);
 	pthread_mutex_unlock(&mutex);
 	return ret;
@@ -239,10 +281,15 @@ out:
 
 int main(void)
 {
-	struct graph G = {0, 0, 10};
+	struct graph G = {0, 0, 0, 10};
+	group_collector col = {0, 0, 0, 10};
+	col.list = (int *) malloc(sizeof(int) * col.capacity);
 	int i;
 	G.gr = malloc(sizeof(struct ver) * G.capacity);
 	reading("/etc/role", &G);
+	G.used = (int *) malloc(sizeof(int) * G.capacity);
+	memset(G.used, 0, sizeof(int) * G.capacity);
+
 	for(i = 0; i < G.size; i++) {
 		int j;
 		printf("%llu: ", G.gr[i].gid);
@@ -250,5 +297,12 @@ int main(void)
 			printf("%llu, ", G.gr[i].list[j]);
 		printf("\n");
 	}
+	dfs(&G, 514, &col);
+	for(i = 0; i < col.size; i++)
+		printf("%llu ", col.list[i]);
+	printf("\n");
+	free(col.list);
+	free_all(&G);
+
 	return 0;
 }
