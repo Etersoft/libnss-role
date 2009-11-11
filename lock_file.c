6641af9d1ffbb6d4f70e19903947f70c15e832de
Pavel Shilovsky <piastry@altlinux.org>
2009-11-11 10:47:15 +0300

    fixes

--- lock_file.c
+++ lock_file.c
@@ -7,14 +7,14 @@
 #include <stdio.h>
 #include <errno.h>
 #include <string.h>
-#include <Role/parser.h>
+#include <Role/glob.h>
 
 static int get_pid(char *buf, pid_t *pid)
 {
 	if (sscanf(buf, "%u", pid) < 1)
-		return IO_ERROR;
+		return LIBROLE_IO_ERROR;
 
-	return OK;
+	return LIBROLE_OK;
 }
 
 static int check_link_count(const char *file)
@@ -22,14 +22,14 @@ static int check_link_count(const char *file)
 	struct stat sb;
 
 	if (stat (file, &sb) != 0) {
-		return UNKNOWN_ERROR;
+		return LIBROLE_UNKNOWN_ERROR;
 	}
 
 	if (sb.st_nlink != 2) {
-		return UNKNOWN_ERROR;
+		return LIBROLE_UNKNOWN_ERROR;
 	}
 
-	return OK;
+	return LIBROLE_OK;
 }
 
 
@@ -66,17 +66,17 @@ static int do_lock(const char *file, const char *lock)
 	if (fd == -1) {
 		unlink(file);
 		errno = EINVAL;
-		return IO_ERROR;
+		return LIBROLE_IO_ERROR;
 	}
 	len = read(fd, buf, sizeof (buf) - 1);
 	close(fd);
 	if (len <= 0) {
 		unlink(file);
 		errno = EINVAL;
-		return IO_ERROR;
+		return LIBROLE_IO_ERROR;
 	}
 	buf[len] = '\0';
-	if ((result = get_pid(buf, &pid)) != OK) {
+	if ((result = get_pid(buf, &pid)) != LIBROLE_OK) {
 		unlink(file);
 		errno = EINVAL;
 		return result;
@@ -84,17 +84,16 @@ static int do_lock(const char *file, const char *lock)
 	if (kill(pid, 0) == 0) {
 		unlink(file);
 		errno = EEXIST;
-		return UNKNOWN_ERROR;
+		return LIBROLE_UNKNOWN_ERROR;
 	}
 	if (unlink(lock) != 0) {
 		unlink(file);
-		return IO_ERROR;
+		return LIBROLE_IO_ERROR;
 	}
 
 	result = 0;
-	if ((link(file, lock) == 0) && (check_link_count(file) == OK)) {
-		result = OK;
-	}
+	if ((link(file, lock) == 0) && (check_link_count(file) == LIBROLE_OK))
+		result = LIBROLE_OK;
 
 	unlink(file);
 	return result;
@@ -105,17 +104,18 @@ int librole_lock(const char *file)
 	char lock[1024];
 
 	if (snprintf(lock, sizeof lock, "%s.lock", file) < 1)
-		return UNKNOWN_ERROR;
+		return LIBROLE_UNKNOWN_ERROR;
 
 	return do_lock(file, lock);
 }
 
-void librole_unlock(const char *file)
+int librole_unlock(const char *file)
 {
 	char lock[1024];
 
 	if (snprintf(lock, sizeof lock, "%s.lock", file) < 1)
-		return UNKNOWN_ERROR;
+		return LIBROLE_UNKNOWN_ERROR;
 
 	unlink(lock);
+	return LIBROLE_OK;
 }
