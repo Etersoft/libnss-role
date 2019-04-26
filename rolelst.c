60af981aecae23015b5dd59201478fbf0a520fa5
Vitaly Lipatov <lav@etersoft.ru>
2019-04-26 20:16:41 +0300

    rolelst: add -n arg to print gid instead of group names

--- rolelst.c
+++ rolelst.c
@@ -10,17 +10,22 @@
 
 struct option rolelst_opt[] = {
 	{"help", no_argument, 0, 'h'},
+	{"number", no_argument, 0, 'n'},
 	{"version", no_argument, 0, 'v'}
 };
 
-static int parse_options(int argc, char **argv)
+static int parse_options(int argc, char **argv, int *numeric_flag)
 {
 	int c, opt_ind;
-	while((c = getopt_long(argc, argv, "hv", rolelst_opt, &opt_ind)) != -1) {
+	*numeric_flag = 0;
+	while((c = getopt_long(argc, argv, "hnv", rolelst_opt, &opt_ind)) != -1) {
 		switch(c) {
 			case 'h':
 				fprintf(stderr, "Usage: rolelst\n  -h [ --help ]\t\t produce help message\n\n");
 				return 0;
+			case 'n':
+				*numeric_flag = 1;
+				break;
 			case 'v':
 				printf("rolelst is the utility for libnss_role version %s\n",
 				       LIBNSS_ROLE_VERSION);
@@ -34,14 +39,27 @@ static int parse_options(int argc, char **argv)
 	return 1;
 }
 
+int print_group(int gid)
+{
+	int result;
+	char gr_name[LIBROLE_MAX_NAME];
 
+	result = librole_get_group_name(gid, gr_name, LIBROLE_MAX_NAME);
+	if (result != LIBROLE_OK) {
+		librole_print_error(result);
+		return result;
+	}
+	printf("%s", gr_name);
+	return LIBROLE_OK;
+}
 
 int main(int argc, char **argv) {
 	struct librole_graph G = {0,0,0,10};
+	int numeric_flag;
 	int result = LIBROLE_OK;
 	int i;
 	
-	if (!parse_options(argc, argv))
+	if (!parse_options(argc, argv, &numeric_flag))
 		goto exit;
 	
 	result = librole_graph_init(&G);
@@ -57,21 +75,22 @@ int main(int argc, char **argv) {
 	
 	for(i = 0; i < G.size; i++) {
 		int j;
-		char gr_name[LIBROLE_MAX_NAME];
-		result = librole_get_group_name(G.gr[i].gid, gr_name, LIBROLE_MAX_NAME);
-		if (result != LIBROLE_OK) {
-			librole_print_error(result);
-			goto exit;
-		}
-		printf("%s:", gr_name);
-
-		for(j = 0; j < G.gr[i].size; j++) {
-			result = librole_get_group_name(G.gr[i].list[j], gr_name, LIBROLE_MAX_NAME);
-			if (result != LIBROLE_OK) {
-				librole_print_error(result);
+		if (numeric_flag) {
+			printf("%u:", G.gr[i].gid);
+		} else {
+			if (print_group(G.gr[i].gid) != LIBROLE_OK)
 				goto exit;
+			putchar(':');
+		}
+		
+		for(j = 0; j < G.gr[i].size; j++) {
+			if (numeric_flag) {
+				printf(" %u", G.gr[i].list[j]);
+			} else {
+				putchar(' ');
+				if (print_group(G.gr[i].list[j]) != LIBROLE_OK)
+					goto exit;
 			}
-			printf(" %s", gr_name);
 		}
 		printf("\n");
 	}
