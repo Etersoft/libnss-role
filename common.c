7da9fbfbff83be8e1cab2d36ee855720c67e875e
Vitaly Lipatov <lav@etersoft.ru>
2019-10-07 23:15:33 +0300

    move librole_get_user_name from common place

--- common.c
+++ common.c
@@ -222,46 +222,6 @@ int librole_get_gid(const char *gr_name, gid_t *ans)
 }
 
 
-/* get username by uid */
-int librole_get_user_name(uid_t uid, char *user_name, size_t user_name_size)
-{
-    size_t bufsize;
-    void *buffer;
-    struct passwd pwd;
-    struct passwd* pwd_ptr;
-    int err, result;
-
-    buffer = get_buffer(_SC_GETPW_R_SIZE_MAX, &bufsize);
-    if (!buffer)
-    return LIBROLE_MEMORY_ERROR;
-
-    for (int i = 0 ; i < BUFFERCOUNT ; i++) {
-        err = getpwuid_r(uid, &pwd, buffer, bufsize, &pwd_ptr);
-        if (err != ERANGE)
-            break;
-        result = librole_realloc_buffer(&buffer, &bufsize);
-        if (result != LIBROLE_OK)
-            return result;
-    }
-
-    if  (err != 0) {
-        free(buffer);
-        return errno_to_result(err);
-    }
-
-    if (!pwd_ptr) {
-        free(buffer);
-        return LIBROLE_NO_SUCH_GROUP;
-    }
-
-    if (user_name) {
-        strncpy(user_name, pwd_ptr->pw_name, user_name_size);
-        user_name[user_name_size - 1 ] = '\0';
-    }
-
-    free(buffer);
-    return LIBROLE_OK;
-}
 
 void librole_print_error(int result)
 {
