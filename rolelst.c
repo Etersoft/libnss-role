715393937f76abe61405c2dd96ea11d63d6ca748
Igor Chudov <nir@altlinux.org>
2020-03-16 14:19:17 +0400

    Add /etc/role.d parsing functionality to rolelst utility

--- rolelst.c
+++ rolelst.c
@@ -7,18 +7,24 @@
 
 #include "role/parser.h"
 #include "role/version.h"
+#include "role/fileop.h"
 
 struct option rolelst_opt[] = {
     {"help", no_argument, 0, 'h'},
     {"numeric", no_argument, 0, 'n'},
-    {"version", no_argument, 0, 'v'}
+    {"version", no_argument, 0, 'v'},
+    {"verbose", no_argument, 0, 'V'}
 };
 
-static int parse_options(int argc, char **argv, int *numeric_flag)
+struct rolelst_settings {
+    int numeric_flag;
+    int verbose_mode;
+};
+
+static int parse_options(int argc, char **argv, struct rolelst_settings *settings)
 {
     int c, opt_ind;
-    *numeric_flag = 0;
-    while((c = getopt_long(argc, argv, "hnv", rolelst_opt, &opt_ind)) != -1) {
+    while((c = getopt_long(argc, argv, "hnvV", rolelst_opt, &opt_ind)) != -1) {
         switch(c) {
             case 'h':
                 fprintf(stdout, "Usage: rolelst [-hnv]\n");
@@ -28,14 +34,21 @@ static int parse_options(int argc, char **argv, int *numeric_flag)
                     "\t-n [ --numeric]\t\tprint gid instead of group names\n");
                 fprintf(stdout,
                     "\t-v [ --version]\t\tprint roledel version being used\n");
+                fprintf(stdout,
+                    "\t-V [ --verbose]\t\tprint extra information about role sources");
                 fprintf(stdout, "\n");
+                fflush(stdout);
                 return 0;
             case 'n':
-                *numeric_flag = 1;
+                settings->numeric_flag = 1;
+                break;
+            case 'V':
+                settings->verbose_mode = 1;
                 break;
             case 'v':
                 printf("rolelst is the utility for libnss_role version %s\n",
                        LIBNSS_ROLE_VERSION);
+                fflush(stdout);
                 return 0;
             case '?':
                 return 0;
@@ -47,11 +60,13 @@ static int parse_options(int argc, char **argv, int *numeric_flag)
 }
 
 int main(int argc, char **argv) {
+    struct rolelst_settings settings;
+    settings.numeric_flag = 0;
+    settings.verbose_mode = 0;
     struct librole_graph G;
-    int numeric_flag;
     int result;
 
-    if (!parse_options(argc, argv, &numeric_flag))
+    if (!parse_options(argc, argv, &settings))
         return 0;
 
     result = librole_graph_init(&G);
@@ -61,10 +76,26 @@ int main(int argc, char **argv) {
     result = librole_reading(LIBROLE_CONFIG, &G);
     if (result != LIBROLE_OK)
         goto exit;
+    if (0 != settings.verbose_mode) {
+        printf("# Settings read from /etc/role:\n");
+        fflush(stdout);
+        result = librole_writing("/dev/stdout", &G, settings.numeric_flag);
+        if (LIBROLE_OK != result) {
+            goto exit;
+        }
+    }
 
-    result = librole_writing("/dev/stdout", &G, numeric_flag);
-    if (result != LIBROLE_OK)
+    /* Don't check return code in order to retain previous utility
+     * behavior */
+    result = librole_get_directory_files(LIBROLE_CONFIG_DIR, &G);
+    if (0 != settings.verbose_mode) {
+        printf("# Resulting settings merged with /etc/role.d entries\n");
+        fflush(stdout);
+    }
+    result = librole_writing("/dev/stdout", &G, settings.numeric_flag);
+    if (LIBROLE_OK != result) {
         goto exit;
+    }
 
 exit:
     librole_print_error(result);
