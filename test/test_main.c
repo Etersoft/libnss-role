5f29157b6613937212ab50432ceeaf82e3c50d02
Igor Chudov <nir@altlinux.org>
2020-03-18 19:12:16 +0400

    Transform standalone program into full-fledged unit test

--- test/test_main.c
+++ test/test_main.c
@@ -15,58 +15,18 @@
 
 static void test_main(void **state) {
     (void) state;
-    int result;
     struct librole_graph G;
 
-    result = librole_graph_init(&G);
-    if (result != LIBROLE_OK)
-        goto exit;
+    assert_int_equal(librole_graph_init(&G), LIBROLE_OK);
+    assert_int_equal(librole_reading("test/role.source", &G), LIBROLE_OK);
+    assert_int_equal(librole_writing("/dev/stdout", &G, 0), LIBROLE_OK);
+    assert_int_equal(librole_writing("/dev/stdout", &G, 1), LIBROLE_OK);
+    assert_int_equal(librole_writing("/dev/stdout", &G, 0), LIBROLE_OK);
+    assert_int_equal(librole_writing("/dev/stdout", &G, 1), LIBROLE_OK);
+    assert_int_equal(librole_writing("test/role.test.new", &G, 0), LIBROLE_OK);
+    assert_int_equal(librole_writing("test/role.test.add", &G, 0), LIBROLE_OK);
+    assert_int_equal(librole_writing("test/role.test.del", &G, 0), LIBROLE_OK);
 
-    result = librole_reading("test/role.source", &G);
-    if (result != LIBROLE_OK)
-        goto exit;
-
-
-    result = librole_writing("/dev/stdout", &G, 0);
-    if (result != LIBROLE_OK)
-        goto exit;
-
-    result = librole_writing("/dev/stdout", &G, 1);
-    if (result != LIBROLE_OK)
-        goto exit;
-
-/* TODO
-{
-    int k;
-    struct librole_ver col;
-    librole_ver_init(&col);
-    librole_dfs(&G,4,&col);
-    for (k = 0; 
-}
-*/
-
-    result = librole_writing("/dev/stdout", &G, 0);
-    if (result != LIBROLE_OK)
-        goto exit;
-
-    result = librole_writing("/dev/stdout", &G, 1);
-    if (result != LIBROLE_OK)
-        goto exit;
-
-    result = librole_writing("test/role.test.new", &G, 0);
-    if (result != LIBROLE_OK)
-        goto exit;
-
-    result = librole_writing("test/role.test.add", &G, 0);
-    if (result != LIBROLE_OK)
-        goto exit;
-
-    result = librole_writing("test/role.test.del", &G, 0);
-    if (result != LIBROLE_OK)
-        goto exit;
-
-exit:
-    librole_print_error(result);
     librole_graph_free(&G);
 }
 
