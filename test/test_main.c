7e9754cb93185e071727f72f007f29d7c0f1bad4
Igor Chudov <nir@altlinux.org>
2020-03-23 14:58:22 +0400

    Fix for freeing allocated memory

--- test/test_main.c
+++ test/test_main.c
@@ -20,13 +20,22 @@ static void test_drop_quotes(void **state) {
     /* Create mutable memory area for test string */
     char *immutable_line[] = { "\"role_name\"" };
     size_t line_length = strlen(*immutable_line);
-    char *mutable_line;
-    mutable_line = calloc(sizeof(char), line_length + 1);
+    char *mutable_line = calloc(line_length + 1, sizeof(char));
+    assert_non_null(mutable_line);
+    /* drop_quotes increments the pointer making it impossible to free
+     * the memory so it is needed to store the original pointer to
+     * be able to free the memory block allocated */
+    char *orig_ptr = mutable_line;
     strncpy(mutable_line, *immutable_line, line_length);
+    strcpy(mutable_line, *immutable_line);
 
     assert_string_equal(mutable_line, "\"role_name\"");
     drop_quotes(&mutable_line);
     assert_string_equal(mutable_line, "role_name");
+
+    free(orig_ptr);
+    mutable_line = NULL;
+    orig_ptr = NULL;
 }
 
 static void test_parse_line(void **state) {
@@ -34,11 +43,14 @@ static void test_parse_line(void **state) {
     assert_int_equal(librole_graph_init(&G), LIBROLE_OK);
     char *immutable_line[] = { "users:\"tftp\",named" };
     size_t line_length = strlen(*immutable_line);
-    char *mutable_line;
-    mutable_line = calloc(sizeof(char), line_length + 1);
+    char *mutable_line = calloc(line_length + 1, sizeof(char));
+    assert_non_null(mutable_line);
     strncpy(mutable_line, *immutable_line, line_length);
 
     parse_line(mutable_line, &G);
+
+    free(mutable_line);
+    mutable_line = NULL;
 }
 
 static void test_main(void **state) {
