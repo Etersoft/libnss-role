9e12f651c7a306caa3e77349cdea75597da49f16
Evgeny Sinelnikov <sin@altlinux.org>
2021-07-12 10:38:19 +0400

    Add support of system roles management

--- src/roleadd.c
+++ src/roleadd.c
@@ -20,6 +20,7 @@
  * USA.
  */
 #include <stdio.h>
+#include <string.h>
 #include <stdlib.h>
 #include <getopt.h>
 #include <unistd.h>
@@ -33,7 +34,8 @@ struct option rolelst_opt[] = {
     {"set", no_argument, 0, 's'},
     {"skip-missing-groups", no_argument, 0, 'm'},
     {"version", no_argument, 0, 'v'},
-    {"file", no_argument, 0, 'f'}
+    {"system", no_argument, 0, 'S'},
+    {"file", required_argument, 0, 'f'}
 };
 
 static void print_help(void)
@@ -48,17 +50,20 @@ static void print_help(void)
     fprintf(stdout,
         "\t-v [ --version]\t\tprint roleadd version being used\n");
     fprintf(stdout,
-        "\t-f [ --file ]\tadd role to file in /etc/role.d/\n");
+        "\t-f [ --file=file.role ]\tchange role in additional file /etc/role.d/file.role\n");
+    fprintf(stdout,
+        "\t-S [ --system ]\tchange system ROLE in additional file /etc/role.d/ROLE.role");
     fprintf(stdout, "\n");
 }
 
-static int parse_options(int argc, char **argv, int *set_flag, int *skip_flag, int *roled_flag)
+static int parse_options(int argc, char **argv, int *set_flag, int *skip_flag, int *roled_flag, char **roled_file, int *system_role_flag)
 {
     int c, opt_ind;
     *set_flag = 0;
     *skip_flag = 0;
     *roled_flag = 0;
-    while((c = getopt_long(argc, argv, "hmsvf", rolelst_opt, &opt_ind)) != -1) {
+    *system_role_flag = 0;
+    while((c = getopt_long(argc, argv, "hmsvf:S", rolelst_opt, &opt_ind)) != -1) {
         switch(c) {
             case 'h':
                 print_help();
@@ -71,6 +76,10 @@ static int parse_options(int argc, char **argv, int *set_flag, int *skip_flag, i
                 break;
             case 'f':
                 *roled_flag = 1;
+                *roled_file = strdup(optarg);
+                break;
+            case 'S':
+                *system_role_flag = 1;
                 break;
             case 'v':
                 printf("roleadd is the utility for libnss_role version %s\n",
@@ -87,14 +96,19 @@ static int parse_options(int argc, char **argv, int *set_flag, int *skip_flag, i
 
 int main(int argc, char **argv) {
     struct librole_graph G;
-    int result, i, set_flag, skip_flag, roled_flag;
+    int result, set_flag, skip_flag, roled_flag, system_role_flag;
     struct librole_ver new_role;
-    const char *filename = NULL;
+    char *filename = NULL;
 
-    if (!parse_options(argc, argv, &set_flag, &skip_flag, &roled_flag))
+    if (!parse_options(argc, argv, &set_flag, &skip_flag, &roled_flag, &filename, &system_role_flag))
         return 0;
 
     if (optind >= argc) {
+        print_help();
+        return 1;
+    }
+
+    if (roled_flag && system_role_flag) {
         print_help();
         return 1;
     }
@@ -104,7 +118,20 @@ int main(int argc, char **argv) {
         goto exit;
 
     if (roled_flag) {
-        filename = argv[optind++];
+        result = librole_validate_filename_from_dir(filename);
+        if (result != LIBROLE_OK)
+            goto exit;
+
+        librole_read_file_from_dir(LIBROLE_CONFIG_DIR, filename, &G);
+    } else if (system_role_flag) {
+        int filename_sz = strlen(argv[optind]) + strlen(LIBROLE_ROLE_EXTENSION) + 1;
+
+        filename = malloc(filename_sz);
+        if (filename == NULL)
+            goto exit;
+
+        strncpy(filename, argv[optind], filename_sz);
+        strncat(filename, LIBROLE_ROLE_EXTENSION, strlen(LIBROLE_ROLE_EXTENSION));
 
         result = librole_validate_filename_from_dir(filename);
         if (result != LIBROLE_OK)
@@ -126,12 +153,14 @@ int main(int argc, char **argv) {
     else
         result = librole_role_add(&G, new_role);
 
-    if (result == LIBROLE_OK && roled_flag)
-        result = librole_write_dir(filename, "roleadd", &G);
+    if (result == LIBROLE_OK && (roled_flag || system_role_flag))
+        result = librole_write_dir(filename, "roleadd", &G, 1);
     else if (result == LIBROLE_OK)
-        result = librole_write("roleadd", &G);
+        result = librole_write("roleadd", &G, 1);
 
 exit:
+    if (filename != NULL)
+        free(filename);
     librole_print_error(result);
     librole_graph_free(&G);
     return result;
